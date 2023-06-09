ae611d0195c8f3601853a309a18442f29515074e
Jason Woods <jwoods@appliedsystems.com>
2023-06-09 13:14:28 -0500

    refactor: update to use powers array

--- csv_to_ff.py
+++ csv_to_ff.py
@@ -3,12 +3,11 @@
 import csv
 import pyperclip
 
-command = ""
-
 with open('hero_stats.csv', 'r', encoding='utf-8') as f:
     reader = csv.reader(f, delimiter=',')
 
+    powers = []
     for row in reader:
-        command += ' '.join(row) + ' '
+        powers.append(' '.join(row))
 
-pyperclip.copy(command.rstrip(' '))
+    pyperclip.copy(' '.join(powers))
